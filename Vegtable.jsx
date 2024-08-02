457938df4b0cec340f292c558d5d52d7e34eeab7
sanjay9361 <sanjaysenthil177@gmail.com>
2024-08-01 20:05:36 -0700

    fghjk

--- Vegtable.jsx
+++ Vegtable.jsx
@@ -5,58 +5,94 @@ function Vegtable() {
 
   const [picture, photo] = useState([{
 
-    img: "https://preview.colorlib.com/theme/vegefoods/images/product-1.jpg  "
+
+
+    img: "https://preview.colorlib.com/theme/vegefoods/images/product-1.jpg  ",
+    price: "$50"
   },
 
   {
-    img: "https://preview.colorlib.com/theme/vegefoods/images/product-2.jpg"
+    img: "https://preview.colorlib.com/theme/vegefoods/images/product-2.jpg",
+    price: "$100"
   },
   {
-    img: "https://preview.colorlib.com/theme/vegefoods/images/product-3.jpg"
+    img: "https://preview.colorlib.com/theme/vegefoods/images/product-3.jpg",
+    price: "$10"
   },
   {
-    img: "https://preview.colorlib.com/theme/vegefoods/images/product-4.jpg"
+    img: "https://preview.colorlib.com/theme/vegefoods/images/product-4.jpg",
+    price: "$40"
   },
   {
-    img: "https://preview.colorlib.com/theme/vegefoods/images/product-5.jpg"
+    img: "https://preview.colorlib.com/theme/vegefoods/images/product-5.jpg",
+    price: "$60"
   },
   {
-    img: "https://preview.colorlib.com/theme/vegefoods/images/product-6.jpg"
+    img: "https://preview.colorlib.com/theme/vegefoods/images/product-6.jpg",
+    price: "$70"
   },
   {
-    img: "https://preview.colorlib.com/theme/vegefoods/images/product-7.jpg"
+    img: "https://preview.colorlib.com/theme/vegefoods/images/product-7.jpg",
+    price: "$40"
   },
   {
-    img: "https://preview.colorlib.com/theme/vegefoods/images/product-8.jpg"
+    img: "https://preview.colorlib.com/theme/vegefoods/images/product-8.jpg",
+    price: "$30"
   }
 
 
+  
+
   ])
 
+
+  const [isTrue, setTrue] = useState([])
+
+
+
+  function add() {
+    photo(picture+1)
+  }
+
+  function decrement() {
+    photo(picture-1)
+  }
+
+
+  function myfunc(item){
+    isTrue.push(item);
+    console.log(isTrue);
+   
+  }
 
   return (
     <>
       <div className="container">
         <div className="row justify-content-around">
-<div className="col-12 text-center">
- <i>Featured Products</i>
-  <h1>Our Products</h1>
-  <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia</p>
-</div>
+          <div className="col-12 text-center">
+            <i>Featured Products</i>
+            <h1>Our Products</h1>
+            <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia</p>
+          </div>
 
 
           {picture.map(item => (
 
 
-            <div className="col-md-3 mt-5 ">
-              <div className="card" style={{ width: ' 15rem' }}>
-                <img src={item.img} className="card-img-top" alt="..." />
+            <div className="col-md-3 mt-5">
+              <div className="card" style={{ width: ' 15rem' }} >
+                <img src={item.img} className="card-img-top"  />
                 <div className="card-body">
-                  <p className="card-text"></p>
+                  <p className="card-text text-center" >{item.price}</p>
+                  <button onClick={add} >+</button>
+                  <button onClick={decrement} >-</button><br /><br />
+
+
+                  <button onClick={()=>myfunc(item)}>submit</button>
                 </div>
 
 
-              </div>
+              </div>  
 
 
 
--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 
 import Navbar from "./Navbar"
 import Backroundimg from "./Backroundimg"
-import Fruit from "../Fruit"
+// import Fruit from "../Fruit"
 import Vegtable from "../Vegtable"
 import Date from "../Date"
-import Customer from "../Customer"
-import Icon from "../Icon"
+// import Customer from "../Customer"
+// import Icon from "../Icon"
 import Email from "../Email"
 import End from "../End"
 function App() {
@@ -19,11 +19,11 @@ function App() {
 
 <Navbar/><br />
    <Backroundimg/> <br /> 
-<Fruit/><br /><br />
+{/* <Fruit/><br /><br /> */}
 <Vegtable/><br /><br />
 <Date/><br />
-<Customer/><br /><br />
-<Icon/><br /><br /><br /><br />
+{/* <Customer/><br /><br /> */}
+{/* <Icon/><br /><br /><br /><br /> */}
 <Email/><br /><br />
 <End/>
     </>
